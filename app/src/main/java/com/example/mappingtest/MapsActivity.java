d02730e759b75ebbb35b074493e4d3aa5651f039
WalidSafi <32139490+WalidSafi@users.noreply.github.com>
2020-12-06 23:10:49 -0500

    Added Firebase, Device Location class

--- app/src/main/java/com/example/mappingtest/MapsActivity.java
+++ app/src/main/java/com/example/mappingtest/MapsActivity.java
@@ -1,10 +1,19 @@
 package com.example.mappingtest;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
+import android.widget.Toast;
 
 import java.net.*;
 import java.io.*;
@@ -16,11 +25,14 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
 
@@ -30,22 +42,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     ArrayList<String> variables = new ArrayList<String>();
     ArrayList<String> names = new ArrayList<String>();
     ArrayList<String> serverResponse = new ArrayList<String>();
+    protected LocationManager locationManager;
+    Location loc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+       ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+        loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        String lat = String.valueOf(loc.getLatitude());
+        String longit = String.valueOf(loc.getLongitude());
+        System.out.println(longit + " : " + lat);
+
         // Clients variables to send to the sever, Changed this when testing with different emulators for testing
         // Basically implement getting the devices location here, probably using the API if possible, or another method
         // Using the EditText view we can get the name (Time stamp we can add on server side only if needed)
-        variables.add("42.116334");
-        variables.add("-73.541");
-        variables.add("Safaga");
+        variables.add(lat);
+        variables.add(longit);
+        variables.add("yetst33111");
+
+
+        //variables.add("49.13455");
+        //variables.add("-73.33134");
+        //variables.add("Obama");
 
         Thread updateThread = new Thread(new periodicUpdates());
         updateThread.start();
 
+    }
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
@@ -61,9 +115,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         for (int i = 0; i < serverResponse.size(); i = i + 3) {
 
 
-           latHolder = serverResponse.get(i);
-           longHolder = serverResponse.get(i+1);
-           names.add(serverResponse.get(i+2));
+            latHolder = serverResponse.get(i);
+            longHolder = serverResponse.get(i+1);
+            names.add(serverResponse.get(i+2));
 
             double lt = Double.parseDouble(latHolder);
             double lg = Double.parseDouble(longHolder);
@@ -94,10 +148,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
     public void buttonTest(View v) {
-        variables.set(0,"45.716334");
+       variables.set(0,"38.716334");
         variables.set(1,"-72.541");
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        String lat = String.valueOf(location.getLatitude());
+        String longit = String.valueOf(location.getLongitude());
+        variables.set(0,lat);
+        variables.set(1, longit);
+
+        LocationHelper helper = new LocationHelper(
+                location.getLongitude(),
+                location.getLatitude()
+                //  location.getLongitude(),
+                // location.getLatitude()
+        );
+        FirebaseDatabase.getInstance().getReference("Current location")
+                .setValue(helper).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MapsActivity.this, "Location not saved", Toast.LENGTH_SHORT);
+                } else {
+                    Toast.makeText(MapsActivity.this, "Location saved ", Toast.LENGTH_SHORT );
+                }
+            }
+        });
+
     }
 
     // The thread for getting input,
@@ -110,7 +190,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             try
             {
-                // 10.0.2.2 is localhost for an emulator
+                // 10.0.2.2 is localhost for an emulator 5556
+                //Socket s = new Socket("179.168.0.11", 9877);
                 Socket s = new Socket("10.0.2.2", 5556);
 
                 // Creating input and output streams
@@ -158,7 +239,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    public void testingprint(){
-        System.out.println("Hello From testing print function");
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
 }
